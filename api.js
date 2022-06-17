f2ed9d41e597e710796c70103ca20aa84753acbe
AsianCawFeePi <darryllee1999@gmail.com>
2022-06-17 21:00:45 +0000

    Change dotenv to deal with crontab

--- api.js
+++ api.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv");
+dotenv.config({ path: __dirname + '/.env' });
+
 var app = express();
 var bodyParser = require("body-parser");
 
@@ -7,7 +9,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello")
+    res.send("Guo Lun")
 })
 
 app.use("/copypasta", require("./copypastaController"))
