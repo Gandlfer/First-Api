af552557479c6662a7b642842435a060c95c16c7
AsianCawFee <darryllee1999@gmail.com>
2022-06-18 02:02:16 +0800

    Change env name

--- skinsController.js
+++ skinsController.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var mongo = require("mongodb")
 var MongoClient = mongo.MongoClient
-const dbUrl = "mongodb://" + process.env.USER + ":" + process.env.PWD + "@" + process.env.MONGODBIP + ":" + process.env.MONGODBPORT
+const dbUrl = "mongodb://" + process.env.USERID + ":" + process.env.PASSWORD + "@" + process.env.MONGODBIP + ":" + process.env.MONGODBPORT
 var db;
 
 MongoClient.connect(dbUrl, (err, con) => {
